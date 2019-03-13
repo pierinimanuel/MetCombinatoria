09bf2d2160576761807a3a7b34a632f754d5d842
manuel.melchiorri7846 <manuel.melchiorri7846@hotmail.com>
2019-03-13 18:03:13 +0100

    esercizio finito ma non funzionante

--- CalcoliCombinatori/UnitTest1.cs
+++ CalcoliCombinatori/UnitTest1.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace CalcoliCombinatori
+namespace CalcoliCombinatori.Test
 {
 	[TestClass]
-	public class UnitTest1
+	public class CalcoliCombinatori
 	{
-		[TestMethod]
-		public void TestMethod1()
+		[DataTestMethod]
+		[DataRow(5, 120)]
+		[DataRow(-4, 24)]
+		[DataRow(0, 1)]
+		[DataRow(1, 1)]
+		[DataRow(10, 3628800)]
+		[DataRow(20, 2432902008176640000)]
+		public void TestFattoriale(int num, long fattorialeAsp)
 		{
-
+			long fattoriale = EquazioniLibrary.CalcoliCombinatori.Fattoriale(num);
+			Assert.AreEqual(fattorialeAsp, fattoriale);
 		}
 	}
 }
--- EquazioniLibrary/CalcoliCombinatori.cs
+++ EquazioniLibrary/CalcoliCombinatori.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace EquazioniLibrary
 {
-    public class CalcoliCombinatori
-    {
-    }
+	public static class CalcoliCombinatori
+	{
+		public static long Fattoriale(int num)
+		{
+			if (num < 0)
+			{
+				num = num * (-1);
+			}
+			long fattoriale = 1;
+			for (int i = num; i > 0; i--)
+			{
+				fattoriale = fattoriale * i;
+			}
+
+			return fattoriale;
+		}
+	}
 }
